ace0c08776926ea3853f2d76368c0ca63c222f49
KateLIU55 <liadam364@gmail.com>
2025-07-23 21:53:05 -0700

    Modify CourseEnroll.jsx, adding hooks

--- src/components/student/CourseEnroll.jsx
+++ src/components/student/CourseEnroll.jsx
@@ -3,18 +3,20 @@ import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 import { REGISTRAR_URL } from '../../Constants';
 import Messages from '../Messages';
+import SelectTerm from "../../SelectTerm";
 
 const CourseEnroll = (props) => {
 
   // student adds a course to their schedule
-
+  const [year, setYear] = useState('');
+  const [semester, setSemester] = useState('');
   const [sections, setSections] = useState([]);
   const [message, setMessage] = useState('');
 
   const fetchSections = async () => {
     // get list of open sections for enrollment
     try {
-      const response = await fetch(`${REGISTRAR_URL}/sections/open`,
+      const response = await fetch(`${REGISTRAR_URL}/open?year=${year}&semester=${semester}`,
         {
           method: 'GET',
           headers: {
@@ -40,7 +42,42 @@ const CourseEnroll = (props) => {
     fetchSections();
   }, []);
 
+  const enroll = async (sectionId) => {
+    try {
+      const res = await fetch(`${REGISTRAR_URL}/studentSchedule`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': sessionStorage.getItem('jwt'),
+        },
+        body: JSON.stringify({ sectionId }),
+      });
+      if (res.ok) {
+        setMessage('Course added successfully');
+        fetchSections();
+      } else {
+        setMessage('Error adding course: ' + res.status);
+      }
+    } catch (err) {
+      setMessage('Network error: ' + err);
+    }
+  };
 
+  const confirmAdd = (sec) => {
+    confirmAlert({
+      title: 'Confirm to add',
+      message: `Are you sure you want to add ${sec.courseId} - ${sec.title}?`,
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => enroll(sec.id),
+        },
+        {
+          label: 'No',
+        },
+      ],
+    });
+  };
 
   const headers = ['section No', 'year', 'semester', 'course Id', 'section', 'title', 'building', 'room', 'times', 'instructor', ''];
 
@@ -52,6 +89,30 @@ const CourseEnroll = (props) => {
         The last column is an "Add" button that when clicked will first confirm that user want to add
         the course, then adds the course to the students schedule.
       </p>
+      <SelectTerm onTermSelect={({ year, semester }) => { setYear(year); setSemester(semester); }} />
+      <Messages response={message} />
+      <table>
+        <thead>
+        <tr>{headers.map(h => <th key={h}>{h}</th>)}</tr>
+        </thead>
+        <tbody>
+        {sections.map(sec => (
+            <tr key={sec.id}>
+              <td>{sec.sectionNo || sec.id}</td>
+              <td>{sec.year}</td>
+              <td>{sec.semester}</td>
+              <td>{sec.courseId}</td>
+              <td>{sec.section}</td>
+              <td>{sec.title}</td>
+              <td>{sec.building}</td>
+              <td>{sec.room}</td>
+              <td>{sec.times}</td>
+              <td>{sec.instructor}</td>
+              <td><button onClick={() => confirmAdd(sec)}>Add</button></td>
+            </tr>
+        ))}
+        </tbody>
+      </table>
 
     </div>
   );
